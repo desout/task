a729eb974a6b3cef5244778eed7d34516652a410
Andrey Anelkin <35991006+desout@users.noreply.github.com>
2018-11-30 14:25:23 +0300

    string are Welcome! example of stream

--- main.js
+++ main.js
@@ -1,67 +1,71 @@
-function calculate(form) {
-    var elements = form.elements;
-    var firstValueInt = elements.firstValue.value
-        .split('')
-        .map(function (item) { return parseInt(item); });
-    var secondValueInt = elements.secondValue.value
-        .split('')
-        .map(function (item) { return parseInt(item); });
-    console.log(firstValueInt.join() + "/" + secondValueInt.join() + "= " + divElements(firstValueInt.slice(), secondValueInt.slice()).join());
-}
-function removeLeadingZeros(value) {
-    for (var _i = 0, value_1 = value; _i < value_1.length; _i++) {
-        var element = value_1[_i];
-        if (element !== 0) {
-            break;
-        }
-        value.shift();
-    }
-    return value;
-}
-function findBiggestValue(firstValue, secondValue) {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var calculate = function () {
+    var firstValue = '4214215';
+    var secondValue = '5211';
+    console.log(firstValue + "/" + secondValue + "= " + divElements(firstValue, secondValue));
+};
+var divElements = function (firstValue, secondValue) {
+    //stream examples
+    // const readableStream = new Stream.Readable();
+    // readableStream.push('push');
+    // readableStream.push('push');
+    // readableStream.push('push');
+    // readableStream.push(null);
+    // console.log(readableStream.read().toString());
+    return subValues(firstValue, secondValue);
+};
+var findBiggestValue = function (firstValue, secondValue) {
     if (firstValue.length === secondValue.length) {
-        return firstValue.some(function (element, index) {
-            return element > secondValue[index];
-        });
+        for (var i = 0; i < firstValue.length; i++) {
+            if (firstValue[i] < secondValue[i]) {
+                return false;
+            }
+        }
+        return true;
     }
     else {
         return firstValue.length > secondValue.length;
     }
-}
-function createBigger(firstValue, secondValue) {
+};
+var createBigger = function (firstValue, secondValue) {
     var count = 0;
     while (!findBiggestValue(firstValue, secondValue)) {
-        firstValue.push(0);
+        firstValue += '0';
         count++;
     }
     return { array: firstValue, count: count };
-}
-function addLeadingZeros(value, count) {
+};
+var addLeadingZero = function (value) {
+    return "0" + value;
+};
+var addZeros = function (value, count) {
+    var zerosString = "";
     for (var i = 0; i < count; i++) {
-        value.unshift(0);
+        zerosString += '0';
     }
-    return value;
-}
-function addZeros(value, count) {
-    for (var i = 0; i < count; i++) {
-        value.push(0);
-    }
-    return value;
-}
-function subValues(firstValue, secondValue) {
-    secondValue = addLeadingZeros(secondValue, firstValue.length - secondValue.length);
-    var returnValue = [];
-    for (var i = firstValue.length - 1; i >= 0; i--) {
-        if (firstValue[i] < secondValue[i]) {
-            firstValue[i] += 10;
-            firstValue[i - 1]--;
+    return value + zerosString;
+};
+var subFn = function (value, index, subValue) {
+    return value.substr(0, index) + (Number(value[index]) - subValue) + value.substr(index, value.length - index - 1);
+};
+var AddFn = function (value, index, addValue) {
+    return value.substr(0, index) + (Number(value[index]) + addValue) + value.substr(index, value.length - index - 1);
+};
+var subValues = function (firstValue, secondValue) {
+    var subOne = false;
+    for (var i = secondValue.length - 1; i >= 0; i--) {
+        var secondNumber = Number(secondValue[i]);
+        if (subOne) {
+            firstValue = subFn(firstValue, i, 1);
+            subOne = !subOne;
         }
-        returnValue[i] = firstValue[i] - secondValue[i];
+        if (Number(firstValue[i]) < secondNumber) {
+            subOne = true;
+            firstValue = AddFn(firstValue, i, 10);
+        }
+        firstValue = subFn(firstValue, i, secondNumber);
     }
-    return returnValue;
-}
-function divElements(firstValue, secondValue) {
-    firstValue = removeLeadingZeros(firstValue);
-    secondValue = removeLeadingZeros(secondValue);
-    return subValues(firstValue, secondValue);
-}
+    return firstValue;
+};
+calculate();
--- main.ts
+++ main.ts
@@ -1,71 +1,76 @@
-interface FormElements extends HTMLFormElement {
-    firstValue: HTMLInputElement;
-    secondValue: HTMLInputElement;
-}
+import { Stream } from 'stream';
+const calculate = () => {
+  const firstValue = '4214215';
+  const secondValue = '5211';
+  console.log(`${firstValue}/${secondValue}= ${divElements(firstValue, secondValue)}`);
+};
 
+const divElements=(firstValue: string, secondValue: string): string => {
+  //stream examples
+  // const readableStream = new Stream.Readable();
+  // readableStream.push('push');
+  // readableStream.push('push');
+  // readableStream.push('push');
+  // readableStream.push(null);
+  // console.log(readableStream.read().toString());
+  return subValues(firstValue, secondValue);
+};
 
-function calculate(form: HTMLFormElement) {
-    const elements: FormElements = <FormElements> (<any>form.elements);
-    const firstValueInt: Array<number> = elements.firstValue.value
-        .split('')
-        .map((item) => parseInt(item));
-    const secondValueInt: Array<number> = elements.secondValue.value
-        .split('')
-        .map((item) => parseInt(item));
-    console.log(`${firstValueInt.join()}/${secondValueInt.join()}= ${divElements(firstValueInt.slice(),secondValueInt.slice()).join()}`);
-}
-function removeLeadingZeros(value: Array<number>): Array<number> {
-    for(const element of value) {
-        if(element !== 0){
-            break;
-        }
-        value.shift();
+const findBiggestValue = (firstValue: string, secondValue: string): boolean =>{
+  if (firstValue.length === secondValue.length) {
+    for(let i=0; i< firstValue.length; i++){
+      if(firstValue[i] < secondValue[i]){
+        return false;
+      }
     }
-    return value;
-}
-function findBiggestValue(firstValue: Array<number>, secondValue: Array<number>): boolean {
-    if(firstValue.length === secondValue.length){
-        return firstValue.some((element,index) => {
-            return element > secondValue[index];
-        })
-    } else {
-        return firstValue.length > secondValue.length;
+    return true;
+  } else {
+    return firstValue.length > secondValue.length;
+  }
+};
+const createBigger=(firstValue: string, secondValue: string): { array: string, count: number } => {
+  let count = 0;
+  while (!findBiggestValue(firstValue, secondValue)) {
+    firstValue += '0';
+    count++;
+  }
+  return {array: firstValue, count: count};
+};
+const addLeadingZero=(value: string) => {
+
+  return "0" +value;
+};
+const addZeros=(value: string, count: number): string => {
+  let zerosString = "";
+  for (let i = 0; i < count; i++) {
+    zerosString+='0';
+  }
+  return value + zerosString;
+};
+
+const subFn=(value:string, index: number, subValue: number): string => {
+  return value.substr(0,index) + (Number(value[index])-subValue) + value.substr(index, value.length-index-1);
+};
+const AddFn=(value:string, index: number, addValue: number): string => {
+  return value.substr(0,index) + (Number(value[index])+addValue) + value.substr(index, value.length-index-1);
+};
+
+const subValues=(firstValue: string, secondValue: string): string => {
+  let subOne: boolean = false;
+  for (let i = secondValue.length - 1; i >= 0; i--) {
+    const secondNumber: number =Number(secondValue[i]);
+    if(subOne){
+     firstValue  = subFn(firstValue,i,1);
+     subOne = !subOne;
     }
-}
-function createBigger(firstValue: Array<number>, secondValue: Array<number>): {array : Array<number> , count: number} {
-    let count = 0;
-    while(!findBiggestValue(firstValue,secondValue)){
-        firstValue.push(0);
-        count++;
+    if (Number(firstValue[i]) < secondNumber) {
+      subOne = true;
+      firstValue = AddFn(firstValue,i,10);
+
     }
-    return {array: firstValue,count: count};
-}
-function addLeadingZeros(value: Array<number>, count: number) {
-    for (let i=0; i<count; i++) {
-        value.unshift(0);
-    }
-    return value;
-}
-function addZeros(value: Array<number>, count: number): Array<number> {
-    for (let i=0; i<count; i++) {
-        value.push(0);
-    }
-    return value;
-}
-function subValues(firstValue: Array<number>, secondValue: Array<number>): Array<number> {
-    secondValue = addLeadingZeros(secondValue, firstValue.length-secondValue.length);
-    const returnValue: Array<number> = [];
-    for(let i=firstValue.length-1; i>= 0; i--){
-        if(firstValue[i] < secondValue[i]) {
-            firstValue[i]+=10;
-            firstValue[i-1]--;
-        }
-        returnValue[i] = firstValue[i]-secondValue[i];
-    }
-    return returnValue;
-}
-function divElements(firstValue: Array<number>, secondValue: Array<number>): Array<number> {
-    firstValue =removeLeadingZeros(firstValue);
-    secondValue = removeLeadingZeros(secondValue);
-    return subValues(firstValue,secondValue);
-}
+    firstValue = subFn(firstValue,i,secondNumber)
+  }
+  return firstValue;
+};
+
+calculate();
